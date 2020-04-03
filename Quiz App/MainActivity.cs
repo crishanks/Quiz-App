daa46a10abe658439041da8e5e095e450fb648a7
Cris Hanks <I27761@xactware.com>
2020-04-03 16:38:30 -0600

    first

---
+++ Quiz App/MainActivity.cs
@@ -0,0 +1,28 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+
+namespace Quiz_App
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = false)]
+    public class MainActivity : AppCompatActivity
+    {
+        Android.Support.V7.Widget.Toolbar toolbar;
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+
+            toolbar = (Android.Support.V7.Widget.Toolbar)FindViewById(Resource.Id.toolbar);
+
+            //Setup Toolbar
+            SetSupportActionBar(toolbar);
+            SupportActionBar.Title = "Topics";
+            Android.Support.V7.App.ActionBar actionBar = SupportActionBar;
+            //Halfway through vid.
+        }
+    }
+}
